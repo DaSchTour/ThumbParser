5da300599c86c7f26791367882c347fbf31cb888
Daniel Schuba <dasch@users.mediawiki.org>
2012-07-15 11:57:47 +0000

    ThumbParser - moved hooks like wished in r98351

---
+++ ThumbParser.hooks.php
@@ -0,0 +1,34 @@
+<?php
+class ThumbParserHooks {
+	function efThumbParserFunction_Setup( $parser ) {
+		# Set a function hook associating the "example" magic word with our function
+		$parser->setFunctionHook( 'thumb', 'efThumbParserFunction_Render' );
+		return true;
+	}
+	
+	function efThumbParserFunction_Render( $parser, $param1 = '') {
+		# The parser function itself
+		# The input parameters are wikitext with templates expanded
+		# The output should be wikitext too
+		global $wgThumbLimits, $wgUser;
+		$param1 = strtolower($param1);
+		$parser->disableCache(); # Mark this content as uncacheable
+		$wopt = $wgUser->getOption('thumbsize');
+		switch ($param1) {
+			case 'size':
+				$output = $wgThumbLimits[$wopt];
+				break;
+			case 'px':
+				$output = $wgThumbLimits[$wopt].'px';
+				break;
+			case 'width':
+				$size = intval($wgThumbLimits[$wopt])+10;
+				$strsize = strval($size);
+				$output = $strsize."px";
+				break;
+			default:
+				$output = $wgThumbLimits[$wopt];
+		}
+		return $output;
+	}
+}
--- ThumbParser.php
+++ ThumbParser.php
@@ -27,36 +27,5 @@ $wgExtensionMessagesFiles['ThumbParser'] = $dir . 'ThumbParser.i18n.php';
 $wgExtensionMessagesFiles['ThumbParserMagic'] = $dir . 'ThumbParser.i18n.magic.php';
 
 # Define a setup function
-$wgHooks['ParserFirstCallInit'][] = 'efThumbParserFunction_Setup';
-
-function efThumbParserFunction_Setup( $parser ) {
-	# Set a function hook associating the "example" magic word with our function
-	$parser->setFunctionHook( 'thumb', 'efThumbParserFunction_Render' );
-	return true;
-}
-
-function efThumbParserFunction_Render( $parser, $param1 = '') {
-	# The parser function itself
-	# The input parameters are wikitext with templates expanded
-	# The output should be wikitext too
-	global $wgThumbLimits, $wgUser;
-	$param1 = strtolower($param1);
-	$parser->disableCache(); # Mark this content as uncacheable
-	$wopt = $wgUser->getOption('thumbsize');
-	switch ($param1) {
-		case 'size':
-			$output = $wgThumbLimits[$wopt];
-			break;
-		case 'px':
-			$output = $wgThumbLimits[$wopt].'px';
-			break;
-		case 'width':
-			$size = intval($wgThumbLimits[$wopt])+10;
-			$strsize = strval($size);
-			$output = $strsize."px";
-			break;
-		default:
-			$output = $wgThumbLimits[$wopt];
-	}
-	return $output;
-}
+$wgAutoloadClasses['ThumbParserHooks'] = "$dir/ThumbParser.hooks.php";
+$wgHooks['ParserFirstCallInit'][] = 'ThumbParserHooks::efThumbParserFunction_Setup';
